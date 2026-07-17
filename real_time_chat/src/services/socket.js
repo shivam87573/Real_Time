70770dcc2f2ff13fcf612504ea976a910b572bed
shivam kumar <132760478+shivam87573@users.noreply.github.com>
2026-07-17 11:04:07 -0700

    Update socket.js

--- real_time_chat/src/services/socket.js
+++ real_time_chat/src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5001", {
+export const socket = io("https://real-time-2crc.onrender.com", {
   autoConnect: false,
 });
